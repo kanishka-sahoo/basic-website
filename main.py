155eb5f78affe415078325dd2d0c068788e64e53
kanishka-sahoo <115538551+kanishka-sahoo@users.noreply.github.com>
2022-10-21 20:16:52 +0530

    Updated the main.py for main branch

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 '''
 ~~~~~INSERT TITLE HERE~~~~~
-Author: Kanishka Sahoo
-Date: 2022/10/21
+Author: 420s
 '''
 
 def main():
-    pass
+    pass    # code will go here instead of 'pass'
 
 if __name__ == '__main__':	# checks if program is run as a script or imported as module, and runs only if as script
     main()
