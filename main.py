7ab6c87e8093cb85f820a4f8ab9b98407e2c8e52
kanishka-sahoo <kanishka.sa.hoo@gmail.com>
2022-10-21 11:22:23 +0530

    Added a main file

---
+++ main.py
@@ -0,0 +1,11 @@
+'''
+~~~~~INSERT TITLE HERE~~~~~
+Author: Kanishka Sahoo
+Date: 2022/10/21
+'''
+
+def main():
+    pass
+
+if __name__ == '__main__':	# checks if program is run as a script or imported as module, and runs only if as script
+    main()
