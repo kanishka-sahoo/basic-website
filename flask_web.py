73fceb2ff25b7ce0ee60c00377352a8eece4de75
kanishka-sahoo <kanishka.sa.hoo@gmail.com>
2022-10-27 14:59:56 +0530

    Added the index html page

--- flask_web.py
+++ flask_web.py
@@ -4,7 +4,7 @@ Author: Kanishka Sahoo
 Date: 2022/10/27
 '''
 
-from flask import Flask
+from flask import Flask, redirect, render_template, url_for
 
 # initialise the server
 app = Flask(__name__)
@@ -12,7 +12,7 @@ app = Flask(__name__)
 # Defining the home page of our site
 @app.route("/")  # this sets the route to this page
 def home():
-	return "Hello! this is the main page <h1>HELLO</h1>"  # some basic inline html
+	return render_template('index.html') # some basic inline html
 
 if __name__ == "__main__":  # checks if program is run as a script or imported as module, and runs only if as script
     app.run()   # at 127.0.0.1:5000
