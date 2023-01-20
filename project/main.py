dab0a7486f4b09d457230937596e495a9e6000b4
kanishka-sahoo <kanishka.sa.hoo@gmail.com>
2023-01-20 20:20:48 +0530

    Do it

--- project/main.py
+++ project/main.py
@@ -1,7 +1,4 @@
 from flask import Blueprint, render_template
-from flask_login import login_required, current_user
-from . import db
-
 main = Blueprint('main', __name__)
 
 @main.route('/')
@@ -12,7 +9,4 @@ def index():
 def about_page():
     return render_template("about.html")
 
-@main.route('/profile')
-@login_required
-def profile():
-    return render_template('profile.html', name=current_user.name)
+
